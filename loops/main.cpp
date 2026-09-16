c5af4e1aee1ac0c0b79e5922c3e5f404bb50f221
Ilethami <elijohn072008@gmail.com>
2026-09-16 11:33:09 +0800

    Organized c++ exercise types

--- loops/main.cpp
+++ loops/main.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
+#include <iostream> // iostream(cin,cout etc.)
+#include <iomanip>  //iomanip(setprecision) iomanip(fixed decimals etc.)
+#include <vector>   //arrays
 
 using namespace std;
 // Values
@@ -8,5 +8,4 @@ using namespace std;
 // Main method
 int main()
 {
-    cout << "Ente";
 }
