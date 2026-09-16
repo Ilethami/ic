71b3d5361e71d1f6b6f74a13478af9c2070aa932
Ilethami <elijohn072008@gmail.com>
2026-09-16 11:24:50 +0800

    Organized c++ exercise types

---
+++ loops/f.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// values
+int x;
+int main()
+{
+    while (x < 5)
+    {
+        cout << "The value of x is:" << x << endl;
+        x++;
+    }
+}
---
+++ loops/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+// Values
+
+// Main method
+int main()
+{
+    cout << "Ente";
+}
