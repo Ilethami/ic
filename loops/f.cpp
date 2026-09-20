7458d826953476dcb01b36327c82b99bd7a72b8a
Ilethami <elijohn072008@gmail.com>
2026-09-20 22:21:57 +0800

    Added from laptop

--- loops/f.cpp
+++ loops/f.cpp
@@ -3,12 +3,56 @@
 using namespace std;
 
 // values
-int x;
+int x = 0;
+int y = 0;
+
+int score, total, sat, unsat, exc;
+// Evaluate scores
+int evalScore()
+{
+    if (score >= 0 && score <= 100)
+    {
+        if (score >= 90)
+        {
+            cout << "Score is excellent!" << endl;
+            exc++;
+        }
+        else if (score >= 60)
+        {
+            cout << "Score is satisfactory!" << endl;
+            sat++;
+        }
+        else if (score < 60)
+        {
+            cout << "Score is unsatisfactory!" << endl;
+            unsat++;
+        }
+    }
+    else
+    {
+        cout << "Score is cannot be less than 0 or more than 100" << endl;
+    }
+    return score;
+}
+//  Main method
 int main()
 {
-    while (x < 5)
+    cout << "How many scores do you want to evaluate: ";
+    cin >> x;
+    for (int i = 0; i < x; i++)
     {
-        cout << "The value of x is:" << x << endl;
-        x++;
+        while (y < x)
+        {
+            cout << "Enter score " << y++ << ": ";
+            cin >> score;
+            evalScore();
+            total += score;
+        }
     }
+    cout << "Total score: " << total << endl;
+    cout << "Total satisfactory scores: " << sat << endl;
+    cout << "Total unsatisfactory scores: " << unsat << endl;
+    cout << "Total excellent scores: " << exc << endl;
+
+    return 0;
 }
