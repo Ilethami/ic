baa8790ca2585b6bad11030dab862b9dca6489f5
Ilethami <elijohn072008@gmail.com>
2026-09-20 23:41:07 +0800

    Optimized, organized, and conditioning of errors in code

---
+++ loops/sc.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+// values
+int nofScores = 0;
+int y = 0;
+
+int score, total, sat, unsat, exc;
+// Evaluate scores
+//  Main method
+int main()
+{
+    cout << "How many scores do you want to evaluate: ";
+    cin >> nofScores;
+    for (int i = 0; i < nofScores; i++)
+    {
+        while (nofScores > y)
+        {
+            cout << "Enter score " << ++y << ": ";
+            cin >> score;
+
+            if (score >= 0 && score <= 100)
+            {
+
+                if (score >= 90)
+                {
+                    cout << "Score is excellent!" << endl;
+                    exc++;
+                }
+                else if (score >= 60)
+                {
+                    cout << "Score is satisfactory!" << endl;
+                    sat++;
+                }
+                else if (score < 60)
+                {
+                    cout << "Score is unsatisfactory!" << endl;
+                    unsat++;
+                }
+                total += score;
+            }
+            else
+            {
+                cout << "Invalid score! Please enter a score between 0 and 100." << endl;
+                --y;
+            }
+        }
+        // Break loop if num. of evaluated scores is 0 or less
+        if (nofScores <= y)
+        {
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    // Conditioned only if there are valid scores entered, and score evaluated not equal to 0 or less
+    if (nofScores > 0)
+    {
+
+        cout << "Total score: " << total << endl;
+        cout << "Total satisfactory scores: " << sat << endl;
+        cout << "Total unsatisfactory scores: " << unsat << endl;
+        cout << "Total excellent scores: " << exc << endl;
+    }
+    else
+    {
+        cout << "No scores to evaluate" << endl;
+    }
+
+    return 0;
+}
