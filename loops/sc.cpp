f12e868eac692c949b773e3c8ff2fdbee0bd13b5
Ilethami <elijohn072008@gmail.com>
2026-09-21 09:57:17 +0800

    More error handling in loops/sc.cpp

--- loops/sc.cpp
+++ loops/sc.cpp
@@ -5,8 +5,9 @@ using namespace std;
 // values
 int nofScores = 0;
 int y = 0;
-
+int error = 0;
 int score, total, sat, unsat, exc;
+
 // Evaluate scores in main method
 int main()
 {
@@ -14,7 +15,7 @@ int main()
     cin >> nofScores;
     for (int i = 0; i < nofScores; i++)
     {
-        while (nofScores > y)
+        while (nofScores > y && error <= 3)
         {
             cout << "Enter score " << ++y << ": ";
             cin >> score;
@@ -41,8 +42,11 @@ int main()
             }
             else
             {
-                cout << "Invalid score! Please enter a score between 0 and 100." << endl;
+
+                cout << "Invalid score! Please enter a score between 0 and 100." << endl
+                     << "You have " << 3 - error << " attempts left." << endl;
                 --y;
+                error++;
             }
         }
         // Break loop if num. of evaluated scores is 0 or less
@@ -56,7 +60,7 @@ int main()
         }
     }
     // Conditioned only if there are valid scores entered, and score evaluated not equal to 0 or less
-    if (nofScores > 0)
+    if (nofScores > 0 && error <= 3)
     {
 
         cout << "Total score: " << total << endl;
